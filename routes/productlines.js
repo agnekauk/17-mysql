248321754f50ddf06976c22160d8f8bf11104721
agnekauk <agnenemeikaite@gmail.com>
2022-08-08 17:00:06 +0300

    Add product line done

--- routes/productlines.js
+++ routes/productlines.js
@@ -14,4 +14,60 @@ router.get('/', (req, res, next) => {
     });
 });
 
+router.get('/add', (req, res, next) => {
+    res.render('productlines/add', {
+        productLine: '',
+        textDescription: '',
+        htmlDescription: '',
+        image: ''
+    })
+});
+
+router.post('/add', (req, res, next) => {
+
+    let productLine = req.body.productLine;
+    let textDescription = req.body.textDescription;
+    let htmlDescription = req.body.htmlDescription;
+    let image = req.body.image;
+
+    let errors = false;
+
+    if (productLine.length === 0 ||
+        textDescription.length === 0
+    ) {
+
+        errors = true;
+        req.flash('error', 'Visi laukeliai turi būti užpildyti');
+
+        res.render('productlines/add', {
+            productLine: productLine,
+            textDescription: textDescription,
+            htmlDescription: htmlDescription,
+            image: image
+        })
+    }
+    if (!errors) {
+        let form_data = {
+            productLine: productLine,
+            textDescription: textDescription,
+            htmlDescription: htmlDescription,
+            image: image
+        }
+        databaseConnection.query('INSERT INTO productlines SET ?', form_data, (err, result) => {
+            if (err) {
+                req.flash('error', err);
+                res.render('productlines/add', {
+                    productLine: form_data.productLine,
+                    textDescription: form_data.textDescription,
+                    htmlDescription: form_data.htmlDescription,
+                    image: form_data.image
+                })
+            } else {
+                req.flash('success', 'Produktų linija pridėta');
+                res.redirect('/productlines');
+            }
+        })
+    }
+});
+
 module.exports = router;
